92a7116eaec9fa9e700c92387e2142ebc1566c92
Aaron Holmes <aholmes@pureguru-software.com>
2016-01-31 01:52:28 -0500

    Added browserify to gulp + node modules

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,22 @@ var connect = require("gulp-connect");
 // Opens URL in browser
 var open = require("gulp-open");
 
+// Bundles JS
+var browserify = require('browserify');
+
+// Transforms JSX to JS
+var reactify = require('reactify');
+
+// Use conventional text streams with Gulp
+var source = require('vinyl-source-stream')
+
 var config = {
   port: 10001,
   devBaseUrl: 'http://localhost',
   paths: {
     html: './src/*.html',
+    js: './src/**/*.js',
+    mainJs: './src/main.js',
     dist: './dist'
   }
 };
@@ -38,8 +49,19 @@ gulp.task('html', function(){
     .pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
-  gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function(){
+  browserify(config.paths.mainJs)
+    .transform(reactify)
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(config.paths.dist + '/scripts'))
+    .pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('watch', function(){
+  gulp.watch(config.paths.html, ['html']);
+  gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,3 @@
+var App = console.log("temp test");
+
+module.exports = App;
